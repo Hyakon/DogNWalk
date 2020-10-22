7d561d3ba9c4d32662957b21b24a7f3132fc184a
Hyakon <azn.baoul@gmail.com>
2020-10-22 16:45:11 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Begin destroy"
+puts 'Begin destroy'
 
 Stroll.destroy_all
 Dog.destroy_all
 Dogsitter.destroy_all
 City.destroy_all
 
-puts "Destroy Finished"
+puts 'Destroy Finished'
 
 5.times do |id_t|
-    id_t = id_t + 1
-    puts "Generating Data#{id_t}"
-    City.create(id: id_t, city_name:"city#{id_t}"
+  id_t += 1
+  puts "Generating Data#{id_t}"
+  City.create(id: id_t, city_name:"city#{id_t}")
 end
 
 5.times do |id_t|
-    id_t = id_t + 1
-    puts "Generating Data#{id_t}"
-    Dogsitter.create(id: id_t, name: "sitter_first#{id_t}", city_id:  rand(1..5))
-    Dog.create(id: id_t, name: "dog#{id_t}", city_id:  rand(1..5))
+  id_t += 1
+  puts "Generating Data#{id_t}"
+  Dogsitter.create(id: id_t, name: "sitter_first#{id_t}", city_id:  rand(1..5))
+  Dog.create(id: id_t, name: "dog#{id_t}", city_id:  rand(1..5))
 end
 
 
 
 25.times do |id|
-    Stroll.create(date: Time.now.to_datetime, dog_id: rand(1..5), dogsitter_id: rand(1..5))
-    puts "Generating appointment #{id}"
+  Stroll.create(date: Time.now.to_datetime, dog_id: rand(1..5), dogsitter_id: rand(1..5))
+  puts "Generating appointment #{id}"
 end
 
-puts "Generation complete"
+puts 'Generation complete'
